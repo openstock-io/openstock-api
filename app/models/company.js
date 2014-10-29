ff0eba2de4b1a2521bdc507c85fe0ce3c102c87f
David Jørgensen <david@kaustik.com>
2014-10-29 13:25:09 +0100

    sets shortname optional

--- app/models/company.js
+++ app/models/company.js
@@ -3,8 +3,8 @@ var Schema       = mongoose.Schema;
 
 var companySchema = new Schema({
 	name: {type: String, required: true},
-	shortName: {type: String, required: true},
-	orgNum: {type: String, required: false},
+	shortname: {type: String, required: false},
+	orgnum: {type: String, required: false},
 	index: [{name: String, value: Number}]
 });
 
